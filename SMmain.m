1cda34d5f6430cea1d186632b3f4199825cdd26b
dildevilliers <ddv@sun.ac.za>
2015-03-27 17:50:51 +0200

    Update SMmain to first full version
    Included some basic plot functionality and finished the output
    structures.  This should be the first full (stable?) version of the main
    loop.

--- SMmain.m
+++ SMmain.m
@@ -1,6 +1,6 @@
-function [Ri,Si,Pi] = SMmain(xinit,Sinit,SMopts,Mf,Mc,OPTopts)
+function [Ri,Si,Pi,Ci, Li] = SMmain(xinit,Sinit,SMopts,Mf,Mc,OPTopts)
 
-% Space Mapping main loop 
+% Space Mapping main loop
 
 % Inputs:
 % xinit:    Initial values of input parameters [Nn,1]
@@ -27,8 +27,8 @@ function [Ri,Si,Pi] = SMmain(xinit,Sinit,SMopts,Mf,Mc,OPTopts)
 %   freq:       Array of simulation frequencies [Nm,1] (optional)
 % OPTopts:  Optimization options structure for external loop
 %   Rtype:      Type of response (cell array if more than one needed)
-%               Valid types:
-%               'S11dB' - obvious!
+%               Valid types (so far):
+%               'S11dB' 
 %               'S11complex'
 %   Ni:         Maximum number of iterations
 %   globOpt:    Flag to run PBIL (1 for only first iteration, 2 for all iterations) (default 0)
@@ -53,31 +53,39 @@ function [Ri,Si,Pi] = SMmain(xinit,Sinit,SMopts,Mf,Mc,OPTopts)
 %                   'L2' (L2 norm)
 %   optsFminS:  options for the fminsearch local optimizer
 %   optsPBIL:   options for the PBIL global optimizer
+%   plotIter:   Flag to plot the responses after each iteration
 
 
 % Returns:
 % Ri:   Structure containing the responses at each iteration
 % Si:   Structure containing the surrogates at each iteration
 % Pi:   Structure containing the parameters at each iteration
+% Ci:   Structure containing the costs at each iteration
+% Li:   Structure containing the limiting information at each iteration
 
 % Date created: 2015-03-06
 % Dirk de Villiers and Ryno Beyers
-% Last Modified: 2015-03-12
+% Last Modified: 2015-03-27
 % Updates:
 % 2015-03-06: Write function shell and basic functionality
 % 2015-03-09: Continue with shell and basic functionality
 % 2015-03-10: Continue with shell and basic functionality
 % 2015-03-11: Continue with shell and basic functionality
 % 2015-03-12: Continue with shell and basic functionality
+% 2015-03-13: Continue with shell and basic functionality
+% 2015-03-14: Add MATLAB case to fine model evaluation function
+% 2015-03-27: Add costeq cost function
+%             Add plotIter functionality
 
 
 % Set defaults
-Ni = 10;
+Ni = 10;    % Maximum unmber of iterations
 globOpt = 0;
 M_PBIL = 8;
 globOptSM = 0;
 optsFminS = optimset('display','none');
 optsPBIL = [];
+plotIter = 1;
 
 if isfield(OPTopts,'Ni'), Ni = OPTopts.Ni; end
 if isfield(OPTopts,'globOpt'), globOpt = OPTopts.globOpt; end
@@ -86,6 +94,7 @@ if isfield(OPTopts,'globOptSM'), globOptSM = OPTopts.globOptSM; end
 if isfield(OPTopts,'optsFminS'), optsFminS = OPTopts.optsFminS; end
 if isfield(OPTopts,'M_PBIL'), M_PBIL = OPTopts.M_PBIL; end
 if isfield(OPTopts,'optsPBIL'), optsPBIL = OPTopts.optsPBIL; end
+if isfield(OPTopts,'plotIter'), plotIter = OPTopts.plotIter; end
 
 % Set up models - bookkeeping
 Nq = 0;
@@ -108,10 +117,10 @@ else
 end
 useAllFine = 0;
 if isfield(SMopts,'wk')
-    % Assume user wants to use all calulated fine responses to fit the
+    % Assume user wants to use all calculated fine responses to fit the
     % surrogate...
     useAllFine = 1;
-end 
+end
 
 % Optimize coarse model to find initial alignment position
 % Limit the parameter space
@@ -121,31 +130,22 @@ if isfield(Mc,'xpmin'), Sinit.xpmin = Mc.xpmin; end
 if isfield(Mc,'xpmax'), Sinit.xpmax = Mc.xpmax; end
 
 if globOpt
-    [xinit,fval,exitFlag,output] = PBILreal(@(xi) costSurr(xi,Sinit,OPTopts),Mc.ximin,Mc.ximax,M_PBIL,optsPBIL);
+    [xinit,costSi,exitFlag,output] = PBILreal(@(xi) costSurr(xi,Sinit,OPTopts),Mc.ximin,Mc.ximax,M_PBIL,optsPBIL);
     xinit = reshape(xinit,Nn,1);
 end
-[xi{1}, fval] = fminsearch(@(xi) costSurr(xi,Sinit,OPTopts),xinit,optsFminS);
-% Limit the inputs
-if isfield(Mc,'ximin')
-    minI = xi{1} < Mc.ximin;
-    xi{1}(minI) = Mc.ximin(minI);
-end
-if isfield(Mc,'ximax')
-    maxI = xi{1} > Mc.ximax;
-    xi{1}(maxI) = Mc.ximax(maxI);
-end
-% fval = 0;
-% x{1} = xinit;
+[xi{1}, costSi] = fminsearch(@(xi) costSurr(xi,Sinit,OPTopts),xinit,optsFminS);
 
 % Enter the main loop
 % [Rci,Rfi,Rsi,Rsai,Si] = deal(cell(1,Ni));
 % Rsai is the aligned surrogate, and Rsi the optimized surrogate at each
 % iteration
-cost = fval;
-for ii = 1:Ni
+costC = costSi; % Only calculate the coarse model cost once, at iteration 0
+specF = 0;  % Flag to test if the fine model reached spec
+ii = 0;
+while ii <= Ni && ~specF
+    ii = ii+1;
     Rci{ii} = coarseMod(Mc,xi{ii},Sinit.xp,fc);
     Rfi{ii} = fineMod(Mf,xi{ii});
-    % Build break in here...
     if ii == 1
         for rr = 1:Nr
             if globOptSM > 0, SMopts.globOpt = 1; end
@@ -175,62 +175,124 @@ for ii = 1:Ni
             if isfield(Rci{ii}{rr},'f'), Rsai{ii}{rr}.f = Rci{ii}{rr}.f; end
         end
     end
-    % Do optimization
-    if (ii == 1 && globOpt) || globOpt == 2
-        [xinit,fval,exitFlag,output] = PBILreal(@(xi) costSurr(xi,Si{ii},OPTopts),Mc.ximin,Mc.ximax,M_PBIL,optsPBIL);
-        xinit = reshape(xinit,Nn,1);
+    
+    % Test fine model response
+    costFi = costFunc(Rfi{ii},OPTopts);
+    if costFi == 0  % Exit is spec is reached (will typically not work for eq and never for minimax...)
+        specF = 1;
+    else
+        specF = 0;
+        % Do optimization
+        if (ii == 1 && globOpt) || globOpt == 2
+            [xinit,costSi,exitFlag,output] = PBILreal(@(xi) costSurr(xi,Si{ii}{:},OPTopts),Mc.ximin,Mc.ximax,M_PBIL,optsPBIL);
+            xinit = reshape(xinit,Nn,1);
+        end
+        [xi{ii+1}, costSi] = fminsearch(@(xi) costSurr(xi,Si{ii}{:},OPTopts),xinit,optsFminS);
+        
+        % Check if inputs will be limited, and don't actually limit the optimized
+        % position here.  Important since the SM might move it back within
+        % limits in the next iteration, therefore changing the evaluation
+        % position of the surrogate model and effectively changing the
+        % surrogate parameters!
+        % This can make the optimizer very inefficient.  Check if a parameter
+        % is limited and try to remove it from the optimization space
+        % externally by changing the models.
+        % Return vector with 0 if not, 1 for max and -1 for min...
+        [limF{ii},limC{ii}] = deal(zeros(Nn,1));
+        % Fine model
+        if isfield(Mc,'ximin')
+            minI = xi{ii+1} < Mf.ximin;
+            limF{ii}(minI) = -1;
+        end
+        if isfield(Mc,'ximax')
+            maxI = xi{ii+1} > Mf.ximax;
+            limF{ii}(maxI) = 1;
+        end
+        % Coarse model
+        if isfield(Mc,'ximin')
+            minI = xi{ii+1} < Mc.ximin;
+            limC{ii}(minI) = -1;
+        end
+        if isfield(Mc,'ximax')
+            maxI = xi{ii+1} > Mc.ximax;
+            limC{ii}(maxI) = 1;
+        end
+        % Surrogate model (bit more complicated!)
+        for rr = 1:Nr
+            limS{ii}{rr} = zeros(Nn,1);
+            % First get some defaults or actual values from the surrogate...
+            if ~isfield(Si{ii}{rr},'B')
+                B = eye(Nn);
+            else
+                B = Si{ii}{rr}.B;
+            end
+            if ~isfield(Si{ii}{rr},'c')
+                c = zeros(Nn,1);
+            else
+                c = Si{ii}{rr}.c;
+            end
+            xis = B*xi{ii+1} + c;
+            if isfield(Mc,'ximin')
+                minI = xis < Mc.ximin;
+                limS{ii}{rr}(minI) = -1;
+            end
+            if isfield(Mc,'ximax')
+                maxI = xis > Mc.ximax;
+                limS{ii}{rr}(maxI) = 1;
+            end
+        end
     end
-    [xi{ii+1}, fval] = fminsearch(@(xi) costSurr(xi,Si{ii},OPTopts),xinit,optsFminS);
-    % Limit the inputs
-    if isfield(Mc,'ximin')
-        minI = xi{ii+1} < Mc.ximin;
-        xi{ii+1}(minI) = Mc.ximin(minI);
-    end
-    if isfield(Mc,'ximax')
-        maxI = xi{ii+1} > Mc.ximax;
-        xi{ii+1}(maxI) = Mc.ximax(maxI);
-    end
-
-    cost = [cost, fval];
+    
+    
+    costS{ii} = costSi;
+    costF{ii} = costFi;
     % Make a (crude) log file
-    save SMlog ii xi Rci Rfi Rsi Si cost
-
-    figure(ii)
-    plot(Rfi{ii}{1}.f,Rfi{ii}{1}.r,'k'), grid on, hold on
-    plot(Rci{ii}{1}.f,Rci{ii}{1}.r,'r'), grid on, hold on
-    plot(Rsi{ii}{1}.f,Rsi{ii}{1}.r,'b')
-    plot(Rsai{ii}{1}.f,Rsai{ii}{1}.r,'g')
-%     plot(Rfi{ii}{1}.f,dB20(Rfi{ii}{1}.r),'k'), grid on, hold on
-%     plot(Rci{ii}{1}.f,dB20(Rci{ii}{1}.r),'r'), grid on, hold on
-%     plot(Rsi{ii}{1}.f,dB20(Rsi{ii}{1}.r),'b')
-%     plot(Rsai{ii}{1}.f,dB20(Rsai{ii}{1}.r),'g')
-    xlabel('Frequency [GHz]')
-    ylabel('|S_{11}| [dB]')
-    title(['Iteration ',num2str(ii)])
-    legend('Fine','Coarse','Optimised Surrogate', 'Aligned Surrogate')
-    stdFig
-   
-
+    save SMlog ii xi Rci Rfi Rsi Si costS costF limF limC limS
+    
+    if plotIter
+        figure(ii)
+        for rr = 1:Nr
+            subplot(Nr,1,rr)
+            if isfield(Rci{ii}{rr},'f')
+                plot(Rci{ii}{rr}.f,Rfi{ii}{rr}.r,'k'), grid on, hold on
+                plot(Rci{ii}{rr}.f,Rci{ii}{rr}.r,'r'), grid on, hold on
+                plot(Rsi{ii}{rr}.f,Rsi{ii}{rr}.r,'b')
+                plot(Rsai{ii}{rr}.f,Rsai{ii}{rr}.r,'g')
+                xlabel('Frequency')
+                % Plot the specs...
+            else
+                plot(Rfi{ii}{rr}.r,'k'), grid on, hold on
+                plot(Rci{ii}{rr}.r,'r'), grid on, hold on
+                plot(Rsi{ii}{rr}.r,'b')
+                plot(Rsai{ii}{rr}.r,'g')
+                xlabel('Index')
+                % Plot the specs...
+            end
+            ylabel(OPTopts.Rtype{rr})
+            title(['Iteration ',num2str(ii)])
+            legend('Fine','Coarse','Optimised Surrogate', 'Aligned Surrogate')
+        end
+        
+    end
+    
 end
 
+
+% Handle output structures
 Ri.Rc = Rci;
 Ri.Rf = Rfi;
-Ri.Rs = Rsi;
-Ri.Rsa = Rsai;
+Ri.Rs = Rsi;    % Surrogate after optimization
+Ri.Rsa = Rsai;  % Surrogate before optimization, just after alignment at end of previous iteration
 
 Pi = xi;
 
+Ci.costC = costC;
+Ci.costS = costS;
+Ci.costF = costF;
 
-
-
-
-
-
-
-% Exit if fine model makes spec 
-
-
-
+Li.limC = limC;
+Li.limS = limS;
+Li.limF = limF;
 
 end
 
@@ -279,14 +341,14 @@ if strcmp(M.solver,'CST')
     cst = actxserver('CSTSTUDIO.Application');
     % Get handle to the model
     mws = invoke(cst,'OpenFile',[M.path,M.name,'.cst']);
-%     % Update parameters
-%     for nn = 1:Nn
-%         invoke(mws,'StoreParameter',M.params{nn},xi(nn));
-%     end
-%     invoke(mws,'Rebuild');
-%     % Run simulation
-%     solver = invoke(mws,'Solver');
-%     invoke(solver,'Start');
+    % Update parameters
+    for nn = 1:Nn
+        invoke(mws,'StoreParameter',M.params{nn},xi(nn));
+    end
+    invoke(mws,'Rebuild');
+    % Run simulation
+    solver = invoke(mws,'Solver');
+    invoke(solver,'Start');
     
     % Generate output
     for rr = 1:Nr
@@ -311,8 +373,8 @@ if strcmp(M.solver,'CST')
             Rf{rr}.t = Rtype{rr};
             release(result);
         elseif strcmp(Rtype{rr},'S11complex')
-            resultA = invoke(mws,'Result1D','a1(1)1(1)');    % amplitude of S11 
-            resultP = invoke(mws,'Result1D','p1(1)1(1)');    % amplitude of S11 
+            resultA = invoke(mws,'Result1D','a1(1)1(1)');    % amplitude of S11
+            resultP = invoke(mws,'Result1D','p1(1)1(1)');    % amplitude of S11
             % Get nr of frequency points in the plots
             nRead = invoke(resultA,'GetN');
             [fin,S11in] = deal(zeros(nRead,1));
@@ -340,6 +402,28 @@ if strcmp(M.solver,'CST')
     end
     invoke(mws,'Save');
     invoke(mws,'Quit');
+elseif strcmp(M.solver,'MATLAB')    % If solver is MATLAB
+    Ni = length(M.params);  % This is interpreted as the number of inputs to the function
+    inType = [];
+    for ii = 1:Ni
+        inType = [inType,M.params{ii}];   % Initialise
+    end
+    switch inType
+        case 'xf'
+            Rfi = M.name(xi,f);
+        otherwise
+            Rfi = M.name(xi);
+    end
+    % Distribute the responses
+    % For MATLAB case the model should the return the specified responses
+    % columnwise...
+    for rr = 1:Nr
+        Rf{rr}.r = Rfi(:,rr);
+        Rf{rr}.t = Rtype{rr};
+        if exist('f','var')
+            Rf{rr}.f = f;
+        end
+    end
 end
 end
 
@@ -415,7 +499,7 @@ if strcmp(M.solver,'MATLAB')
     for rr = 1:Nr
         Rc{rr}.r = Rci(:,rr);
         Rc{rr}.t = Rtype{rr};
-        if ~isempty(f)
+        if exist('f','var')
             Rc{rr}.f = f;
         end
     end
@@ -425,7 +509,7 @@ end
 
 
 
-function cost = costSurr(x,S,OPTopts)  
+function cost = costSurr(x,S,OPTopts)
 
 % Function to run the Surrogate model S at x to return the cost as
 % calculated from the information in OPTopts.
@@ -463,9 +547,9 @@ function cost = costFunc(R,GOALS)
 %   goalType:   Cell array of goal types {1,Ng}
 %               Valid types:
 %                   'lt' (Less than)
-%                   'gt' (Greater than) - todo
+%                   'gt' (Greater than) 
 %                   'eq' (equal to) - todo
-%                   'minimax' - todo
+%                   'minimax' 
 %   goalVal:    Cell array of goal values {1,Ng} - same order as goalType
 %   goalWeight: Vector of goal weights [1,Ng] (default equal weights)
 %   goalStart:  Cell array of start of valid goal domain {1,Ng} (optional)
@@ -497,13 +581,13 @@ for gg = 1:Ng
     if isfield(GOALS,'goalWeight'), goalWeight = GOALS.goalWeight{gg}; end
     wSum = wSum + goalWeight;
     
-     % Special case for complex and dB S11 goals...
-     if strcmp(R{1}.t,'S11complex') && strcmp(GOALS.goalResType{gg},'S11dB')
-         Ri = R{1};
-         Ri.r = dB20(R{1}.r);
-     else
-         Ri = R{1};
-     end
+    % Special case for complex and dB S11 goals...
+    if strcmp(R{1}.t,'S11complex') && strcmp(GOALS.goalResType{gg},'S11dB')
+        Ri = R{1};
+        Ri.r = dB20(R{1}.r);
+    else
+        Ri = R{1};
+    end
     tt = 1;
     while tt < Nr
         if strcmp(R{tt}.t,GOALS.goalResType{gg})
@@ -634,7 +718,6 @@ function c0 = costminimax(R,G)
 % not specified the (optional) goal limits will be interpreted as indeces in
 % the vector.
 % The structure G contains:
-%   goalVal:    Goal value
 %   goalStart:  Start of valid goal domain (optional)
 %   goalStop:   Stop of valid goal domain (optional)
 %   errNorm:    Type of error norm to use for optimization
@@ -669,6 +752,58 @@ end
 
 Rvalid = R.r(iStart:iStop);
 c0 = max(Rvalid);
+end
+
+
+function c0 = costeq(R,G)
+% Function to evaluate an 'eq' (equal to) goal function for the
+% given response R.
+% R is a structure containing the response in R.r and the
+% (optional) domain (typically frequency) in R.f.  If the domain is
+% not specified the (optional) goal limits will be interpreted as indeces in
+% the vector.
+% The structure G contains:
+%   goalVal:    Goal value - can be scalar or vector of the same length as
+%               R.r
+%   goalStart:  Start of valid goal domain (optional)
+%   goalStop:   Stop of valid goal domain (optional)
+%   errNorm:    Type of error norm to use for optimization
+%               Valid types:
+%                   'L1' (L1 norm - default)
+%                   'L2' (L2 norm)
+
+Nm = length(R.r);
+if ~isfield(R,'f')
+    if isfield(G,'goalStart')
+        iStart = G.goalStart;
+    else
+        iStart = 1;
+    end
+    if isfield(G,'goalStop')
+        iStop = G.goalStop;
+    else
+        iStop = Nm;
+    end
+else
+    if isfield(G,'goalStart')
+        iStart = find(R.f >= G.goalStart,1);
+    else
+        iStart = 1;
+    end
+    if isfield(G,'goalStop')
+        iStop = find(R.f <= G.goalStart,1,'last');
+    else
+        iStop = Nm;
+    end
+end
+
+Rvalid = R.r(iStart:iStop);
+if length(G.goalVal) == length(R.r)
+    y = Rvalid - G.goalVal(iStart:iStop);
+else
+    y = Rvalid - G.goalVal;
+end
+c0 = Lnorm(y,G.errNorm);
 end
 
 
